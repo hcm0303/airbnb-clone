7c4080e6faf81c99b20ff7473d95a5d90259d9db
Hyunchul Ma <hcm0303@gmail.com>
2020-07-08 14:34:42 +0900

    signup: Form

--- users/forms.py
+++ users/forms.py
@@ -38,3 +38,44 @@ class LoginForm(forms.Form):
     #             raise forms.ValidationError("Password is wrong")
     #     except models.User.DoesNotExist:
     #         pass
+
+
+class SignUpForm(forms.Form):
+
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    # initial = {"first_name": "Nicoas", "last_name": "Serr"}
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists with the email")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        return password1
+
+    def save(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        # create_user: password 암호화
+        user = models.User.objects.create_user(
+            username=email, email=email, password=password
+        )
+        # 추가 정보
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
